370605a9348ba46191166719c7fada0c1160e1c9
chliu.tw@gmail.com <chliu.tw@gmail.com>
2026-03-24 08:30:32 +0800

    feat(web): scaffold eagleeye-web module with Spring Boot + Thymeleaf

---
+++ eagleeye-web/src/main/java/com/eagleeye/web/EagleeyeWebApplication.java
@@ -0,0 +1,11 @@
+package com.eagleeye.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleeyeWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EagleeyeWebApplication.class, args);
+    }
+}
