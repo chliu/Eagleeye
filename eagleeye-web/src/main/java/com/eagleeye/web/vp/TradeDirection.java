248e3fb2e151ca5d15fee52feb58d5c883e65a83
chliu.tw@gmail.com <chliu.tw@gmail.com>
2026-06-08 21:06:33 +0800

    feat(vp): add TradeDirection enum and VpCandle DTO

---
+++ eagleeye-web/src/main/java/com/eagleeye/web/vp/TradeDirection.java
@@ -0,0 +1,3 @@
+package com.eagleeye.web.vp;
+
+public enum TradeDirection { UP, DOWN, NEUTRAL }
---
+++ eagleeye-web/src/main/java/com/eagleeye/web/vp/VpCandle.java
@@ -0,0 +1,3 @@
+package com.eagleeye.web.vp;
+
+public record VpCandle(long time, int open, int high, int low, int close, int volume) {}
